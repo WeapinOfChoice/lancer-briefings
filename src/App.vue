d763d37a9b1f8e14b3b6e5db23f0c5bcf11458e9
William Casey <williamcasey99@gmail.com>
2024-04-25 11:50:30 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "mech": "HA Barbarossa"
         },
         {
           "callsign": "Kal",
