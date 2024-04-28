9ee78e40e9fb4b651cff4beeb4010ccd3ded443a
William Casey <williamcasey99@gmail.com>
2024-04-28 12:03:21 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,36 +109,20 @@ export default {
           "mech": "Amenity"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Hobble",
+          "alias": "Soniya Jackson",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "EMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Atlas"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Ontos",
+          "alias": "N/A",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
-        },
-        {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "mech": "M1-M0"
         },
       ],
       "header": {
@@ -147,7 +131,7 @@ export default {
         "system": "Ardennes-3",
         "gate": "Atlas-Quanokrim",
         "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
+        "headerTitle": "Death's Yearning",
         "headerSubtitle": "Mercenary Company",
         "subheaderTitle": "Crisis Response",
         "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
